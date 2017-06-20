2bfe4850a84145eb283b0118b0203f8f2e3d0fff
alpap <al.pap@live.com>
2017-06-21 00:40:37 +0200

    initial commit

--- Antergos/bin/fixMouse.sh
+++ Antergos/bin/fixMouse.sh
@@ -1,12 +1,11 @@
-device=13
+device=15
 xinput --set-button-map $device 1 3 2 
-properties=$(xinput list-props $device | grep "Scrolling Distance")
+properties=$(xinput list-props $device | grep "Synaptics Scrolling Distance")
 echo $device
 echo $properties
-number=${properties:31:3}
-echo $number
-valueOne=${properties:37:2}
-echo $valueOne
-valueTwo=${properties:41:2}
-echo $valueTwo
-xinput set-prop $device $number -$valueOne -$valueTwo
+Sub=$(echo $properties | cut -d '(' -f 2)
+number=$(echo $Sub | cut -d ')' -f 1)
+echo number=$number
+valueTwo=$(echo $Sub | cut -d ' ' -f 2)
+echo valTwo=$valueTwo
+xinput set-prop $device $number -$valueTwo -$valueTwo
